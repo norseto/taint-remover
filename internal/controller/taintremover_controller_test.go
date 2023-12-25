c8e33395876fda3d9db397ac77c5e7ff7050f1d8
Norihiro Seto <norihiros@acm.org>
2023-12-26 01:33:41 +0900

    Add tests

--- internal/controller/taintremover_controller_test.go
+++ internal/controller/taintremover_controller_test.go
@@ -3,51 +3,175 @@ package controller
 import (
 	"context"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/norseto/taint-remover/api/v1alpha1"
 )
 
-func Test_applyTaintRemoveOnNode(t *testing.T) {
-	// Mock objects
-	ctx := context.TODO()
-	nodeName := "test-node"
-	taintValue := "test-taint"
+var _ = Describe("TaintRemoverReconciler", func() {
+	var (
+		ctx    context.Context
+		client client.Client
+		scheme *runtime.Scheme
+	)
 
-	node := &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: nodeName}, Spec: corev1.NodeSpec{Taints: []corev1.Taint{{Key: taintValue, Value: taintValue, Effect: "NoSchedule"}}}}
-	nodeWithoutTaint := &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: nodeName}, Spec: corev1.NodeSpec{Taints: []corev1.Taint{}}}
+	BeforeEach(func() {
+		ctx = context.TODO()
+		client = k8sClient
+		scheme = runtime.NewScheme()
+	})
 
-	testCases := []struct {
-		desc     string
-		node     *corev1.Node
-		hasError bool
-	}{
-		{
-			desc:     "Happy path",
-			node:     node,
-			hasError: false,
-		},
-		{
-			desc:     "Node without Taint",
-			node:     nodeWithoutTaint,
-			hasError: true,
-		},
-	}
+	Describe("Reconcile", func() {
+		Context("When there are taints and nodes", func() {
+			It("should remove taints from nodes", func() {
+				// Create a TaintRemover object
+				tr := &v1alpha1.TaintRemover{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-taint-remover",
+					},
+					Spec: v1alpha1.TaintRemoverSpec{
+						Taints: []corev1.Taint{
+							{
+								Key:    "foo",
+								Value:  "bar",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				}
+				Expect(client.Create(ctx, tr)).To(Succeed())
 
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			cl := fake.NewClientBuilder().WithRuntimeObjects(tc.node).Build()
-			r := &TaintRemoverReconciler{Client: cl}
-			err := r.applyTaintRemoveOnNode(ctx, tc.node)
-			if tc.hasError {
-				assert.NotNil(t, err)
-				assert.Equal(t, errors.IsNotFound(err), true)
-			} else {
-				assert.Nil(t, err)
-			}
+				// Create a Node object with taints
+				node := &corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-node",
+					},
+					Spec: corev1.NodeSpec{
+						Taints: []corev1.Taint{
+							{
+								Key:    "foo",
+								Value:  "bar",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				}
+				Expect(client.Create(ctx, node)).To(Succeed())
+
+				// Reconcile the TaintRemover object
+				reconciler := &TaintRemoverReconciler{
+					Client: client,
+					Scheme: scheme,
+				}
+				req := reconcile.Request{
+					NamespacedName: types.NamespacedName{
+						Name: tr.Name,
+					},
+				}
+				_, err := reconciler.Reconcile(ctx, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify that the taints have been removed from the node
+				nodeKey := types.NamespacedName{
+					Name: node.Name,
+				}
+				Expect(client.Get(ctx, nodeKey, node)).To(Succeed())
+				Expect(node.Spec.Taints).To(BeEmpty())
+			})
 		})
-	}
-}
+
+		Context("When there are no taints", func() {
+			It("should not remove taints from nodes", func() {
+				// Create a TaintRemover object
+				tr := &v1alpha1.TaintRemover{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-taint-remover",
+					},
+					Spec: v1alpha1.TaintRemoverSpec{
+						Taints: []corev1.Taint{},
+					},
+				}
+				Expect(client.Create(ctx, tr)).To(Succeed())
+
+				// Create a Node object with taints
+				node := &corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-node",
+					},
+					Spec: corev1.NodeSpec{
+						Taints: []corev1.Taint{
+							{
+								Key:    "foo",
+								Value:  "bar",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				}
+				Expect(client.Create(ctx, node)).To(Succeed())
+
+				// Reconcile the TaintRemover object
+				reconciler := &TaintRemoverReconciler{
+					Client: client,
+					Scheme: scheme,
+				}
+				req := reconcile.Request{
+					NamespacedName: types.NamespacedName{
+						Name: tr.Name,
+					},
+				}
+				_, err := reconciler.Reconcile(ctx, req)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify that the taints have not been removed from the node
+				nodeKey := types.NamespacedName{
+					Name: node.Name,
+				}
+				Expect(client.Get(ctx, nodeKey, node)).To(Succeed())
+				Expect(node.Spec.Taints).To(HaveLen(1))
+			})
+		})
+
+		Context("When there are no nodes", func() {
+			It("should not remove taints", func() {
+				// Create a TaintRemover object
+				tr := &v1alpha1.TaintRemover{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-taint-remover",
+					},
+					Spec: v1alpha1.TaintRemoverSpec{
+						Taints: []corev1.Taint{
+							{
+								Key:    "foo",
+								Value:  "bar",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				}
+				Expect(client.Create(ctx, tr)).To(Succeed())
+
+				// Reconcile the TaintRemover object
+				reconciler := &TaintRemoverReconciler{
+					Client: client,
+					Scheme: scheme,
+				}
+				req := reconcile.Request{
+					NamespacedName: types.NamespacedName{
+						Name: tr.Name,
+					},
+				}
+				_, err := reconciler.Reconcile(ctx, req)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
+})
