fa826887bdeda37e8716d56902de354fcc35b40e
Norihiro Seto <norihiros@acm.org>
2025-04-20 05:40:24 +0900

    Fix bug

--- internal/controller/taintremover_controller.go
+++ internal/controller/taintremover_controller.go
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2023 Norihiro Seto
+Copyright (c) 2023-2025 Norihiro Seto
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
