f1ff7fb683e338b68a4159efc49496f68dc2e5a8
Norihiro Seto <norihiros@acm.org>
2025-06-22 16:11:34 +0900

    Add develop environments

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get install -y --no-install-recommends xdg-utils
+
 # Directory ownership - automatically extract from devcontainer.json
 DIRS="/home/vscode/.aws /home/vscode/.kube /usr/local/go /go /tmp/gocache /home/vscode/.cache/JetBrains"
 
@@ -8,3 +11,9 @@ for dir in $DIRS; do
     sudo chown -R vscode:vscode "$dir"
   fi
 done
+
+ln -s $HOME/.bun/bin/bun $HOME/.bun/bin/node
+ln -s $HOME/.bun/bin/bun $HOME/.bun/bin/npm
+ln -s $HOME/.bun/bin/bunx $HOME/.bun/bin/npx
+
+bun install -g @openai/codex
